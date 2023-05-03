ee1b7db71549284b49b4875ecde25a09cbc7e5b7
Isaac Batista <isaacbatst@gmail.com>
2023-05-03 13:32:07 -0300

    add missing export

--- src/domain/entities/Power/GeneralPower/index.ts
+++ src/domain/entities/Power/GeneralPower/index.ts
@@ -2,4 +2,5 @@ export * from './GeneralPowerName';
 export * from './Dodge';
 export * from './IronWill';
 export * from './Medicine';
+export * from './FightStyle';
 export * from './GeneralPowerFactory';
