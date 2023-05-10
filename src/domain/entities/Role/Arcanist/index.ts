683b7a33d8d3b3e5467b43d3b6795b3a2480b859
Isaac Batista <isaacbatst@gmail.com>
2023-05-10 19:38:25 -0300

    export arcanist factories

--- src/domain/entities/Role/Arcanist/index.ts
+++ src/domain/entities/Role/Arcanist/index.ts
@@ -2,3 +2,6 @@ export * from './ArcanistBuider';
 export * from './ArcanistPath/';
 export * from './Arcanist';
 export * from './ArcanistFactory';
+export * from './ArcanistFactoryMage';
+export * from './ArcanistFactorySorcerer';
+export * from './ArcanistFactoryWizard';
