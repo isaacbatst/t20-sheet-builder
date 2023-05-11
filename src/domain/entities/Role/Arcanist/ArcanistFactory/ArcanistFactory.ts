bd28122a0da177516aa819efcda6bc5773f25474
Isaac Batista <isaacbatst@gmail.com>
2023-05-11 00:49:18 -0300

    remove unused attributes

--- src/domain/entities/Role/Arcanist/ArcanistFactory/ArcanistFactory.ts
+++ src/domain/entities/Role/Arcanist/ArcanistFactory/ArcanistFactory.ts
@@ -1,31 +1,15 @@
-import {type EquipmentName} from '../../../Inventory';
-import {type GeneralPowerName} from '../../../Power';
-import {type Attribute} from '../../../Sheet';
 import {type SkillName} from '../../../Skill';
 import {SpellFactory, type SpellName} from '../../../Spell';
 import {type Arcanist} from '../Arcanist';
 import {ArcanistBuilder} from '../ArcanistBuider';
-import {type ArcanistPathName, type ArcanistLineageDraconicDamageType, type ArcanistLineageType, type ArcanistPath, type ArcanisPathWizardFocusName, type ArcanistLineageFactory, ArcanistLineageFactoryDraconic, ArcanistLineageFactoryFaerie, ArcanistLineageFactoryRed} from '../ArcanistPath';
 import {type ArcanistPathFactory} from '../ArcanistPath/ArcanistPathFactory/ArcanistPathFactory';
-import {ArcanistPathFactoryMage} from '../ArcanistPath/ArcanistPathFactory/ArcanistPathFactoryMage';
-import {ArcanistPathFactorySorcerer} from '../ArcanistPath/ArcanistPathFactory/ArcanistPathFactorySorcerer';
-import {ArcanistPathFactoryWizard} from '../ArcanistPath/ArcanistPathFactory/ArcanistPathFactoryWizard';
 
 export type ArcanistFactoryParams = {
 	chosenSkills: SkillName[];
 	initialSpells: SpellName[];
-	pathName: ArcanistPathName;
-	mageSpell?: SpellName;
-	wizardFocus?: EquipmentName;
-	sorcererLineage?: ArcanistLineageType;
-	sorcererLineageDraconicDamageType?: ArcanistLineageDraconicDamageType;
-	sorcererLineageFaerieExtraSpell?: SpellName;
-	sorcererLineageRedExtraPower?: GeneralPowerName;
-	sorcererLineageRedAttribute?: Attribute;
 };
 
 export class ArcanistFactory {
-	protected pathName: ArcanistPathName;
 	private readonly chosenSkills: SkillName[];
 	private readonly initialSpells: SpellName[];
 
@@ -35,7 +19,6 @@ export class ArcanistFactory {
 	) {
 		this.chosenSkills = params.chosenSkills;
 		this.initialSpells = params.initialSpells;
-		this.pathName = params.pathName;
 	}
 
 	make(): Arcanist {
