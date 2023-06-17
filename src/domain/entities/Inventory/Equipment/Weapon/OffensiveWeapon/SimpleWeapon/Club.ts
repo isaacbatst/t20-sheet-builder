95cb19a76499979c5562b8824bf4f80231d24791
Isaac Batista <isaacbatst@gmail.com>
2023-06-17 14:42:15 -0300

    fix typings

--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/SimpleWeapon/Club.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/SimpleWeapon/Club.ts
@@ -1,15 +1,15 @@
 import {Critical} from '../../../../../Attack/Critical';
 import {DiceRoll} from '../../../../../Dice/DiceRoll';
 import {EquipmentName} from '../../../EquipmentName';
-import type {OffensiveWeaponStatic} from '../OffensiveWeaponStatic';
 import {SimpleWeapon} from './SimpleWeapon';
+import {type SimpleWeaponName} from './SimpleWeaponName';
 
 export class Club extends SimpleWeapon {
 	static damage: DiceRoll = new DiceRoll(1, 6);
 	static critical: Critical = new Critical();
-	static equipmentName: EquipmentName = EquipmentName.club;
+	static equipmentName: SimpleWeaponName = EquipmentName.club;
 
 	damage: DiceRoll = Club.damage;
 	critical: Critical = Club.critical;
-	name: EquipmentName = Club.equipmentName;
+	name = Club.equipmentName;
 }
--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/SimpleWeapon/Dagger.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/SimpleWeapon/Dagger.ts
@@ -2,13 +2,14 @@ import {Critical} from '../../../../../Attack/Critical';
 import {DiceRoll} from '../../../../../Dice/DiceRoll';
 import {EquipmentName} from '../../../EquipmentName';
 import {SimpleWeapon} from './SimpleWeapon';
+import {type SimpleWeaponName} from './SimpleWeaponName';
 
 export class Dagger extends SimpleWeapon {
 	static damage = new DiceRoll(1, 4);
 	static critical = new Critical(19);
-	static equipmentName = EquipmentName.dagger;
+	static equipmentName: SimpleWeaponName = EquipmentName.dagger;
 
 	damage: DiceRoll = Dagger.damage;
 	critical: Critical = Dagger.critical;
-	name: EquipmentName = Dagger.equipmentName;
+	name = Dagger.equipmentName;
 }
