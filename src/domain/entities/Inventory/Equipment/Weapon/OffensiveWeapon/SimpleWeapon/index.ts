4484cf2fc871b5eca05f6014fbbd7a47d15bcd5f
Isaac Batista <isaacbatst@gmail.com>
2023-06-17 14:43:40 -0300

    export simple weapon names

--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/SimpleWeapon/index.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/SimpleWeapon/index.ts
@@ -1,5 +1,6 @@
 export * from './SimpleWeapon';
 export * from './SimpleWeapons';
 export * from './SimpleWeaponFactory';
+export * from './SimpleWeaponName';
 export * from './Club';
 export * from './Dagger';
