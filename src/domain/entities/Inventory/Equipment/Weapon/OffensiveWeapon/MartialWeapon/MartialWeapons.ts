59e1a05583f3747e05e77a8e09a9e257a0b2b0da
Yuri Martins <yuri.alessandro@hotmail.com>
2023-08-15 17:07:41 -0300

    export weapons

--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/MartialWeapon/MartialWeapons.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/MartialWeapon/MartialWeapons.ts
@@ -1,12 +1,34 @@
+import {BattleAxe} from './BattleAxe';
+import {Cutlass} from './Cutlass';
+import {Flail} from './Flail';
+import {Halberd} from './Halberd';
+import {HandAndaHalfSword} from './HandAndAHalfSword';
+import {Hatchet} from './Hatchet';
+import {LongBow} from './LongBow';
 import {LongSword} from './LongSword';
 import {type MartialWeaponStatic} from './MartialWeaponStatic';
+import {Pickaxe} from './Pickaxe';
+import {Scimitar} from './Scimitar';
 import {Scythe} from './Scythe';
+import {Trident} from './Trident';
+import {WarHammer} from './WarHammer';
 
 export class MartialWeapons {
 	static getAll(): MartialWeaponStatic[] {
 		return [
+			BattleAxe,
+			Cutlass,
+			Flail,
+			Halberd,
+			HandAndaHalfSword,
+			Hatchet,
+			LongBow,
 			LongSword,
+			Pickaxe,
+			Scimitar,
 			Scythe,
+			Trident,
+			WarHammer,
 		];
 	}
 }
