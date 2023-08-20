0867321e92d4ad1f6adb3025c25f01aeba57e03a
Isaac Batista <isaacbatst@gmail.com>
2023-08-20 11:58:01 -0300

    refactor: weapons lists

--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/ExoticWeapon/ExoticWeapons.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/ExoticWeapon/ExoticWeapons.ts
@@ -1,20 +1,27 @@
 import {BastardSword} from './BastardSword';
 import {ChainofThorns} from './ChainofThorns';
 import {DwarfAxe} from './DwarfAxe';
+import {type ExoticWeaponName} from './ExoticWeaponName';
 import {type ExoticWeaponStatic} from './ExoticWeaponStatic';
 import {Katana} from './Katana';
 import {TauricAxe} from './TauricAxe';
 import {Whip} from './Whip';
 
 export class MartialWeapons {
+	static map: Record<ExoticWeaponName, ExoticWeaponStatic> = {
+		whip: Whip,
+		bastardSword: BastardSword,
+		katana: Katana,
+		dwarfAxe: DwarfAxe,
+		chainofThorns: ChainofThorns,
+		tauricAxe: TauricAxe,
+	};
+
 	static getAll(): ExoticWeaponStatic[] {
-		return [
-			Whip,
-			BastardSword,
-			Katana,
-			DwarfAxe,
-			ChainofThorns,
-			TauricAxe,
-		];
+		return Object.values(this.map);
+	}
+
+	static getByName(name: ExoticWeaponName): ExoticWeaponStatic {
+		return this.map[name];
 	}
 }
--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/FireArmWeapon/FireArmWeapons.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/FireArmWeapon/FireArmWeapons.ts
@@ -1,13 +1,20 @@
 
+import {type FireArmWeaponName} from './FireArmWeaponName';
 import {type FireArmWeaponStatic} from './FireArmWeaponStatic';
 import {Musket} from './Musket';
 import {Pistol} from './Pistol';
 
 export class FireArmWeapons {
+	static map: Record<FireArmWeaponName, FireArmWeaponStatic> = {
+		pistol: Pistol,
+		musket: Musket,
+	};
+
 	static getAll(): FireArmWeaponStatic[] {
-		return [
-			Pistol,
-			Musket,
-		];
+		return Object.values(this.map);
+	}
+
+	static getByName(name: FireArmWeaponName): FireArmWeaponStatic {
+		return this.map[name];
 	}
 }
