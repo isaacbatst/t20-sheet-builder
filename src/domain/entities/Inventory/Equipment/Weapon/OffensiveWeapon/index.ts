04266c9e0139981acea2d7758c97e548eac859a9
Isaac Batista <isaacbatst@gmail.com>
2023-08-20 12:06:13 -0300

    fix: export new weapons

--- src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/index.ts
+++ src/domain/entities/Inventory/Equipment/Weapon/OffensiveWeapon/index.ts
@@ -2,3 +2,5 @@ export * from './OffensiveWeapon';
 export * from './OffensiveWeaponStatic';
 export * from './SimpleWeapon';
 export * from './MartialWeapon';
+export * from './ExoticWeapon';
+export * from './FireArmWeapon';
