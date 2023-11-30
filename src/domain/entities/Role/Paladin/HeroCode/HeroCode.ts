012adb7f346247c9efe45d26da090f3b0cb7dae8
Isaac Batista <isaacbatst@gmail.com>
2023-11-29 23:07:10 -0300

    feat: hero code effect

--- src/domain/entities/Role/Paladin/HeroCode/HeroCode.ts
+++ src/domain/entities/Role/Paladin/HeroCode/HeroCode.ts
@@ -1,9 +1,20 @@
-import {AbilityEffects, type AbilityEffectsInterface} from '../../../Ability';
+import {AbilityEffects, RolePlayEffect, type AbilityEffectsInterface} from '../../../Ability';
 import {RoleAbility} from '../../RoleAbility';
 import {RoleAbilityName} from '../../RoleAbilityName';
 
 export class HeroCode extends RoleAbility {
-	override effects: AbilityEffectsInterface = new AbilityEffects();
+	static description: string = 'Você deve sempre manter'
+	+ ' sua palavra e nunca pode recusar um pedido de'
+	+ ' ajuda de alguém inocente. Além disso, nunca pode'
+	+ ' mentir, trapacear ou roubar. Se violar o código, você'
+	+ ' perde todos os seus PM e só pode recuperá-los a'
+	+ ' partir do próximo dia.';
+
+	override effects: AbilityEffectsInterface = new AbilityEffects({
+		roleplay: {
+			default: new RolePlayEffect(RoleAbilityName.heroCode, HeroCode.description),
+		},
+	});
 
 	constructor() {
 		super(RoleAbilityName.heroCode);
